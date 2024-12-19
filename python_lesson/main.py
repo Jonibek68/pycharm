a97fc0c00722feb2fe44d3c14a3cc44046467f9f
Jonibek68 <jmuzaffarov14@gmail.com>
2024-12-19 16:15:19 +0500

    second project

---
+++ python_lesson/main.py
@@ -0,0 +1,10 @@
+for i in range(int(input())):
+    print(i)
+
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/Jonibek68/pycharm.git
+# git push -u origin main
+
